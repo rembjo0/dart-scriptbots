af19b8860f39ff4bb9db333a21ad4bb9fdb438b7
Bjørn Vidar Remme <bvremme@gmail.com>
2016-10-27 13:24:34 +0200

    Added empty canvas and status output area

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,43 @@
-// Copyright (c) 2016, BjÃ¸rn Vidar Remme. All rights reserved. Use of this source code
+// Copyright (c) 2016, Bjørn Vidar Remme. All rights reserved. Use of this source code
+
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
 
+
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+
+
+  CanvasElement mainCanvas = querySelector('#mainCanvas');
+  CanvasRenderingContext2D ctx = mainCanvas.getContext('2d');
+
+  var resizeToWindow = () {
+    mainCanvas.width = window.innerWidth;
+    mainCanvas.height = window.innerHeight;
+  };
+
+  var clearScreen = () {
+    ctx.save();
+    ctx.setFillColorRgb(200, 200, 200);
+    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+    //ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    ctx.beginPath();
+    ctx.setStrokeColorRgb(0, 0, 255);
+    ctx.lineWidth = 5;
+    ctx.moveTo(0, 0);
+    ctx.lineTo(mainCanvas.width, mainCanvas.height);
+    ctx.stroke();
+    ctx.restore();
+  };
+
+  window.onResize.listen((e) {
+    resizeToWindow();
+    clearScreen();
+  });
+
+
+  resizeToWindow();
+  clearScreen();
 }
+
+
