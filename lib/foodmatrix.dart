9390ca06d82339e39c9ec6c992a43950b66d9376
Bjørn Vidar Remme <bvremme@gmail.com>
2016-12-07 20:39:00 +0100

    fixed memory issue with dirt food cells tracking

--- lib/foodmatrix.dart
+++ lib/foodmatrix.dart
@@ -12,14 +12,20 @@ class FoodMatrix {
   FoodMatrix(int columns, int rows) : _store = new Array2(columns, rows);
 
   void set(int c, int r, double v) {
+    _store.set(c, r, v);
+
+    if (_allDirty) {
+      return;
+    }
+
+    int limit = ((_store.numColumns*_store.numRows)*0.75).toInt();
 
     // If too many dirty cells, then just mark all as dirty and stop tracking.
-    if (!_allDirty && _dirtyCells.length > _store.numColumns*_store.numRows) {
+    if (_dirtyCells.length > limit) {
       _allDirty = true;
       _dirtyCells = [];
     } else {
       //mark as dirty, note... we do not check duplicates, should be few
-      _store.set(c, r, v);
       _dirtyCells.add(new Point(c, r));
     }
   }
